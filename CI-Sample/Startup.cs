a95835d89a3faf7c915af6e8d70cfbf3e522415b
tung-lin <im_fish@msn.com>
2016-07-07 12:14:21 +0800

    CI-Sample第一次更新

---
+++ CI-Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CI_Sample.Startup))]
+namespace CI_Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
